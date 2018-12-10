7f06decbf6d91032fed7df63a3bc5488997f437f
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2018-12-10 22:43:58 +0200

    Added basic collectionView for main screen

--- ItemRental-iOS/ItemRental-iOS/UI/ItemsViewController.swift
+++ ItemRental-iOS/ItemRental-iOS/UI/ItemsViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ItemsViewController: UIViewController {
     // MARK: - Properties
+    
+    var items = [
+                "Item1",
+                "Item2",
+                "Item3",
+                "Item4",
+                "Item5",
+                "Item6",
+                "Item7",
+                "Item8"
+                ]
+    
+    private let cellId = "cellId"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 180, height: 250)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        
+        return collectionView
+    }()
     
     // MARK: - Base class overrides
     
@@ -23,5 +49,43 @@ final class ItemsViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = .gray
+        
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(Padding.p20)
+            $0.bottom.trailing.equalToSuperview().offset(-Padding.p20)
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .white
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ItemsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 200)
     }
 }
---
+++ ItemRental-iOS/ItemRental-iOS/Utils/Padding.swift
@@ -0,0 +1,13 @@
+//
+//  Padding.swift
+//  ItemRental-iOS
+//
+//  Created by Stefan Lupascu on 10/12/2018.
+//  Copyright © 2018 Stefan Lupascu. All rights reserved.
+//
+
+import UIKit
+
+struct Padding {
+    static let p20: CGFloat = 20
+}
