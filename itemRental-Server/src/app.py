ef90f55cbcb82201246809aa38c229f039ddaf07
Danut <ilisei.danut@gmail.com>
2018-12-20 14:12:43 +0200

    * create basic classes

--- itemRental-Server/src/app.py
+++ itemRental-Server/src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from .models import db
+from .models import db, bcrypt
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/item_rental'
+    bcrypt.init_app(app)
     db.init_app(app)
 
     return app
