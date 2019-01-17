1d0e80c8977e00e7a60a24585c01db86c5dcea9f
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2019-01-17 19:21:56 +0200

    Add Category enum

---
+++ ItemRental-iOS/Category.swift
@@ -0,0 +1,34 @@
+//
+//  Category.swift
+//  
+//
+//  Created by Stefan Lupascu on 17/01/2019.
+//
+
+import Foundation
+
+enum Category {
+    case filmAndPhotography
+    case projectorsAndScreens
+    case drones
+    case djEquipment
+    case sports
+    case musical
+    
+    var category: String {
+        switch self {
+        case .filmAndPhotography:
+            return "Film & Photography"
+        case .projectorsAndScreens:
+            return "Projectors and Screens"
+        case .drones:
+            return "Drones"
+        case .djEquipment:
+            return "DJ Equipment"
+        case .sports:
+            return "Sports"
+        case .musicalInstruments:
+            return "Musical"
+        }
+    }
+}
